463e07994e9e87d49b826eed6921a6dcec22e6b6
David Kababyan <110393905+DavidJinius@users.noreply.github.com>
2024-03-15 11:54:21 +0200

    change Speakers view in home screen (#52)

---
+++ iOSDevUK/Presentation/HomeView/SpeakersHorizontalRowView.swift
@@ -0,0 +1,41 @@
+//
+//  SpeakersHorizontalRow.swift
+//  iOSDevUK
+//
+//  Created by David Kababyan on 15/03/2024.
+//
+
+import SwiftUI
+
+struct SpeakersHorizontalRowView: View {
+    let speakers: [Speaker]
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack {
+                Text(AppStrings.speakers).font(.title2).bold()
+                Spacer()
+                NavigationLink(AppStrings.allSpeakers, value: Destination.speakers(speakers.sorted()))
+                    .foregroundStyle(Color(ColorNames.textGrey))
+            }
+            .padding(.horizontal)
+            
+            ScrollView(.horizontal) {
+                LazyHStack(spacing: 8) {
+                    ForEach(speakers) { speaker in
+                        NavigationLink(value: Destination.speaker(speaker)) {
+                            SpeakerCardView(speaker: speaker)
+                                .frame(width: 100)
+                        }
+                    }
+                }
+                .padding(.leading)
+            }
+            .scrollIndicators(.hidden)
+        }
+    }
+}
+
+#Preview {
+    SpeakersHorizontalRowView(speakers: DummyData.speakers)
+}
--- iOSDevUK/Presentation/ViewComponents/SpeakerCardView.swift
+++ iOSDevUK/Presentation/ViewComponents/SpeakerCardView.swift
@@ -9,46 +9,21 @@ import SwiftUI
 
 struct SpeakerCardView: View {
     
-    private let speaker: Speaker
-    private let gradient = Gradient(colors:
-                                        [Color(ColorNames.primary).opacity(1.0),
-                                         Color(ColorNames.primary).opacity(0.8),
-                                         Color(ColorNames.primary).opacity(0.7),
-                                         Color(ColorNames.primary).opacity(0.2),
-                                         Color(ColorNames.primary).opacity(0.1),
-                                         .clear])
-    
-    init(speaker: Speaker) {
-        self.speaker = speaker
-    }
+    let speaker: Speaker
     
     var body: some View {
-        RemoteImageView(url: speaker.imageUrl)
-            .scaledToFit()
-            .cornerRadius(16)
-            .overlay(alignment: .bottom) {
-                ZStack(alignment: .leading) {
-                    Rectangle()
-                        .background(
-                            LinearGradient(gradient: gradient,
-                                           startPoint: .bottom,
-                                           endPoint: .top)
-                        )
-                        .frame(height: 65)
-                        .cornerRadius(15)
-                        .foregroundColor(.clear)
-                    
-                    Text(speaker.name)
-                        .multilineTextAlignment(.leading)
-                        .minimumScaleFactor(0.6)
-                        .foregroundColor(.white)
-                        .lineLimit(2)
-                        .font(.subheadline)
-                        .bold()
-                        .padding(.horizontal, 10)
-                        .padding(.top, 15)
-                }
-            }
+        VStack(spacing: 8) {
+            RemoteImageView(url: speaker.imageUrl)
+                .scaledToFit()
+                .frame(width: 90, height: 90)
+                .clipShape(Circle())
+            
+            Text(speaker.name)
+                .foregroundStyle(Color(ColorNames.textColor))
+                .minimumScaleFactor(0.6)
+                .lineLimit(2, reservesSpace: true)
+                .font(.subheadline)
+        }
     }
 }
 
